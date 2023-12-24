7adbc3511cdd5d2900e1145d03f4c42ed8096bc3
64bitviruz <D4rkw3b0@proton.me>
2023-12-24 05:50:11 +0545

    added new hosting files

---
+++ script.js
@@ -0,0 +1,22 @@
+// Declaring a variable
+let xp = 0;
+let health =100;
+let gold =50;
+// declaring weapon varible 
+let currentWeapon =0;
+let fighting;
+let monsterHealth;
+
+let inventory = ["stick"]; // declaring  array to store weapon for the game in inventory
+// fixed variable are assigned with const
+const button1 = document.querySelector("#button1"); // assigning the value of button1 for id use (#)
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+// assiging the constant variables for the game
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector('#monsterHealth');
