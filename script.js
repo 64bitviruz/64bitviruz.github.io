363eb5d4f0cb9b55f3302c107ae1e530ec780459
64bitviruz <D4rkw3b0@proton.me>
2023-12-28 12:25:27 +0545

    added progress of 28dec

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// Declaring a variable
-let xp = 0;
-let health =100;
-let gold =50;
-// declaring weapon varible 
-let currentWeapon =0;
-let fighting;
-let monsterHealth;
-
-let inventory = ["stick"]; // declaring  array to store weapon for the game in inventory
-// fixed variable are assigned with const
-const button1 = document.querySelector("#button1"); // assigning the value of button1 for id use (#)
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-// assiging the constant variables for the game
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector('#monsterHealth');
-// initialize buttons
-button1.onclick=goStore; //make button1 to browser to the store
-button2.onclick = goCave;// assigning the buttons to work
-button3.onclick = fightDragon;
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -42,90 +16,33 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText =document.querySelector("#monsterHealth");
+const locations = [
+    {   'button text':[],
+        name: "town square",
+        
+        
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// declaring a funtion for the store
-function goStore() {
-    console.log("Going to the store.");
+function update(location) {
 
 }
-function goCave(){
-    console.log("going to the cave");
-}
-function fightDragon(){
-    console.log("Fighting A dragon");
-}
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
 
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText =document.querySelector("#monsterHealth");
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
-
-
-function goCave() {
-  console.log("Going to cave.");
-}
-
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-// changing the button text on click on going to the store
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
-
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText =document.querySelector("#monsterHealth");
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-//adding funtion of goTown with submenus
 function goTown() {
- button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
+
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
@@ -151,4 +68,3 @@ function buyHealth() {
 function buyWeapon() {
 
 }
-
